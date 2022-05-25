e02c519f135e602bd438a46d11652281ea29c5cc
sipusumit <community.sumit@gmail.com>
2022-05-25 17:18:00 +0530

    Fixed server url

--- pages/class/[standard]/[book].js
+++ pages/class/[standard]/[book].js
@@ -1,5 +1,6 @@
 import Footer from "../../../components/Footer";
 import NavBar from "../../../components/NavBar";
+import getUrl from "../../../lib/url";
 
 export async function getServerSideProps(context){
   const book = context.params.book;
@@ -8,7 +9,7 @@ export async function getServerSideProps(context){
       return {notFound: true}
   }
 
-  let response = await fetch(`http://localhost:3000/api/${standard}/${book}`)
+  let response = await fetch(`${getUrl()}/api/${standard}/${book}`)
   let data = await response.json()
   if(!data.success){
     return{
