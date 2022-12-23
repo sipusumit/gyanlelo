3d22c192086cae697cc4b5251bee1a4a32e52e16
Sumit <88932296+sipusumit@users.noreply.github.com>
2022-12-23 13:54:06 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,13 @@ export default function Home() {
         <title>GyanLelo</title>
         <meta name="description" content="GyanLelo | A free Ncert solutions for class 7 to 10" />
       </Head>
-      <iframe src="https://discord.com/widget?id=987714820239396904&theme=dark" width="350" height="500" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+      <div>
+        <NavBar/>
+          Home
+        <Footer/>
+      </div>
     </div>
   )
 }
+
+//<iframe src="https://discord.com/widget?id=987714820239396904&theme=dark" width="350" height="500" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
